1cb4ca29bd292eefa45e70286ec957332e49de49
Ahmed Samir <ahmedsemido14@gmail.com>
2025-12-24 16:10:11 +0200

    Adding Register ViewModel

---
+++ UsersApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [MinLength(8, ErrorMessage = "Password must be at least 6 characters long.")]
+        [MaxLength(50, ErrorMessage = "Password cannot exceed 50 characters.")]
+        [Compare("ConfirmPassword", ErrorMessage = "Passwords do not match.")]
+        public string Password { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please confirm your password.")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;    
+    }
+}

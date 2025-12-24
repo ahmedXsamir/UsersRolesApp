fe873cdc879670c4636c1c4d7281f31eb943f214
Ahmed Samir <ahmedsemido14@gmail.com>
2025-12-24 16:15:04 +0200

    Adding ChangePassword ViewModel

--- UsersApp/ViewModels/ChangePasswordViewModel.cs
+++ UsersApp/ViewModels/ChangePasswordViewModel.cs
@@ -3,11 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace UsersApp.ViewModels
 {
-    public class RegisterViewModel
+    public class ChangePasswordViewModel
     {
-        [Required(ErrorMessage = "Name is required.")]
-        public string Name { get; set; } = null!;
-
         [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         public string Email { get; set; } = null!;
@@ -16,12 +13,13 @@ namespace UsersApp.ViewModels
         [DataType(DataType.Password)]
         [MinLength(8, ErrorMessage = "Password must be at least 6 characters long.")]
         [MaxLength(50, ErrorMessage = "Password cannot exceed 50 characters.")]
-        [Compare("ConfirmPassword", ErrorMessage = "Passwords do not match.")]
-        public string Password { get; set; } = null!;
+        [DisplayName("New Password")]
+        [Compare("ConfirmNewPassword", ErrorMessage = "Passwords do not match.")]
+        public string NewPassword { get; set; } = null!;
 
         [Required(ErrorMessage = "Please confirm your password.")]
         [DataType(DataType.Password)]
-        [DisplayName("Confirm Password")]
-        public string ConfirmPassword { get; set; } = null!;    
+        [DisplayName("Confirm New Password")]
+        public string ConfirmNewPassword { get; set; } = null!;
     }
 }

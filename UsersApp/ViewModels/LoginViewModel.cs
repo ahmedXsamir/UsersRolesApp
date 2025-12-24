81e01be24ea17b2f308f9efe1757fa711f830b1f
Ahmed Samir <ahmedsemido14@gmail.com>
2025-12-24 16:05:02 +0200

    Adding Login ViewModel

---
+++ UsersApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Display(Name = "Remember Me?")]
+        public bool RememberMe { get; set; }
+    }
+}
